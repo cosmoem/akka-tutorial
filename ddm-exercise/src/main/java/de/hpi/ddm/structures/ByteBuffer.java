7b517fdb163151962132096a7094940b36eafee7
Frieda Gruse <fr.ida@gmx.de>
2021-05-24 16:08:28 +0200

    byte buffer concurrent hash map

--- ddm-exercise/src/main/java/de/hpi/ddm/structures/ByteBuffer.java
+++ ddm-exercise/src/main/java/de/hpi/ddm/structures/ByteBuffer.java
@@ -2,17 +2,18 @@ package de.hpi.ddm.structures;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ByteBuffer {
     // message ID to (chunk number to chunk message bytes)
     private final Map<Long, Map<Integer, byte[]>> messageMap;
 
     public ByteBuffer() {
-        this.messageMap = new HashMap<>();
+        this.messageMap = new ConcurrentHashMap<>();
     }
 
     public void saveChunksToMap(Long messageId, int offset, byte[] bytes) {
-        messageMap.computeIfAbsent(messageId, k -> new HashMap<>());
+        messageMap.computeIfAbsent(messageId, k -> new ConcurrentHashMap<>());
         messageMap.get(messageId).put(offset, bytes);
     }
 
